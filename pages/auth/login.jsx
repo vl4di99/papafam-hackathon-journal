55b08cfe04d1727ee636c11174984fccbf5ef08e
Monwar <monwar259@gmail.com>
2022-06-25 21:48:13 +0100

    Implemented AuthGuard

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -2,6 +2,7 @@ import { Button, Container, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import Link from 'next/link';
 import React, { useContext, useState } from 'react';
+import AuthGuard from '../../components/AuthGuard';
 import { UserContext } from '../../context/UserContext';
 
 const Login = () => {
@@ -16,64 +17,66 @@ const Login = () => {
   } 
   
   return (
-    <Container className='shadow'>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <Box mb={2}>
-          <TextField 
-            onChange={(e) => setEmail(e.target.value)}
-            label="Email" 
-            variant="filled" 
-            fullWidth
-            type="email"
-            value={email}
-          />
-        </Box>
-        <Box mb={2}>
-          <TextField 
-            onChange={(e) => setPassword(e.target.value)}
-            label="Password" 
-            variant="filled"   
-            fullWidth
-            type="password"
-            value={password}
-          />
-        </Box>
-        <Typography my={2} color="red" align='center'>
-          {loginError}
-        </Typography>
-        <Button
-          fullWidth
-          type="submit"
-          variant="contained"
-        >
-          Login
-        </Button>
-      </form>
-      <Box my={2}>
-        <Button
-          onClick={signInWithGoogle}
-          color='secondary'
-          fullWidth
-          type="submit"
-          variant="contained"
-        >
-          Sign in with Google
-        </Button>
-      </Box>
-      <Box my={2}>
-        <Link href="/auth/register">
+    <AuthGuard isLoggedIn={false}>
+      <Container className='shadow'>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <Box mb={2}>
+            <TextField 
+              onChange={(e) => setEmail(e.target.value)}
+              label="Email" 
+              variant="filled" 
+              fullWidth
+              type="email"
+              value={email}
+            />
+          </Box>
+          <Box mb={2}>
+            <TextField 
+              onChange={(e) => setPassword(e.target.value)}
+              label="Password" 
+              variant="filled"   
+              fullWidth
+              type="password"
+              value={password}
+            />
+          </Box>
+          <Typography my={2} color="red" align='center'>
+            {loginError}
+          </Typography>
           <Button
-            color='info'
             fullWidth
             type="submit"
             variant="contained"
           >
-            Create an account
+            Login
           </Button>
-        </Link>
-      </Box>
-    </Container>
+        </form>
+        <Box my={2}>
+          <Button
+            onClick={signInWithGoogle}
+            color='secondary'
+            fullWidth
+            type="submit"
+            variant="contained"
+          >
+            Sign in with Google
+          </Button>
+        </Box>
+        <Box my={2}>
+          <Link href="/auth/register">
+            <Button
+              color='info'
+              fullWidth
+              type="submit"
+              variant="contained"
+            >
+              Create an account
+            </Button>
+          </Link>
+        </Box>
+      </Container>
+    </AuthGuard>
   )
 }
 
--- pages/auth/register.jsx
+++ pages/auth/register.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 import { Button, Container, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system'
 import { UserContext } from '../../context/UserContext';
+import AuthGuard from '../../components/AuthGuard';
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -16,60 +17,62 @@ const Register = () => {
   }
 
   return (
-    <Container className='shadow'>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
-        <Box mb={2}>
-          <TextField 
-            onChange={(e) => setName(e.target.value)}
-            label="Name" 
-            variant="filled" 
+    <AuthGuard isLoggedIn={false}>
+      <Container className='shadow'>
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <Box mb={2}>
+            <TextField 
+              onChange={(e) => setName(e.target.value)}
+              label="Name" 
+              variant="filled" 
+              fullWidth
+              value={name}
+            />
+          </Box>
+          <Box mb={2}>
+            <TextField 
+            onChange={(e) => setEmail(e.target.value)}
+              label="Email" 
+              variant="filled" 
+              fullWidth
+              type="email"
+              value={email}
+            />
+          </Box>
+          <Box mb={2}>
+            <TextField 
+              onChange={(e) => setPassword(e.target.value)}
+              label="Password" 
+              variant="filled"   
+              fullWidth
+              type="password"
+              value={password}
+            />
+          </Box>
+          <Box mb={2}>
+            <TextField 
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              label="Confirm Password" 
+              variant="filled"   
+              fullWidth
+              type="password"
+              value={confirmPassword}
+            />
+          </Box>
+          <Typography my={2} color="red" align='center'>
+            {registerError}
+          </Typography>
+          <Button
             fullWidth
-            value={name}
-          />
-        </Box>
-        <Box mb={2}>
-          <TextField 
-           onChange={(e) => setEmail(e.target.value)}
-            label="Email" 
-            variant="filled" 
-            fullWidth
-            type="email"
-            value={email}
-          />
-        </Box>
-        <Box mb={2}>
-          <TextField 
-            onChange={(e) => setPassword(e.target.value)}
-            label="Password" 
-            variant="filled"   
-            fullWidth
-            type="password"
-            value={password}
-          />
-        </Box>
-        <Box mb={2}>
-          <TextField 
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            label="Confirm Password" 
-            variant="filled"   
-            fullWidth
-            type="password"
-            value={confirmPassword}
-          />
-        </Box>
-        <Typography my={2} color="red" align='center'>
-          {registerError}
-        </Typography>
-        <Button
-          fullWidth
-          type="submit"
-          variant="contained"
-        >
-          Register
-        </Button>
-      </form>
-    </Container>
+            type="submit"
+            variant="contained"
+          >
+            Register
+          </Button>
+        </form>
+      </Container>
+    </AuthGuard>
   )
 }
 
