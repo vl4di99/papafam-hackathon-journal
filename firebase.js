5f283b57345894f0f42dbdedea4d50e0ca17f218
Monwar <monwar259@gmail.com>
2022-06-25 19:01:22 +0100

    Setup user context + login page

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,3 +14,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export {
+  app, 
+  auth,
+  provider
+}
--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import { Button, Container, TextField, Text, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React, { useContext, useState } from 'react'
+import { UserContext } from '../../context/UserContext'
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { signIn, signInWithGoogle, user, loginError } = useContext(UserContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signIn(email, password);
+  } 
+  
   return (
-    <div>Login</div>
+    <Container className='shadow'>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <Box mb={2}>
+          <TextField 
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email" 
+            variant="filled" 
+            fullWidth="100%"
+            type="email"
+            className
+            value={email}
+          />
+        </Box>
+        <Box mb={2}>
+          <TextField 
+            onChange={(e) => setPassword(e.target.value)}
+            label="Password" 
+            variant="filled"   
+            fullWidth="100%"
+            type="password"
+            value={password}
+          />
+        </Box>
+        <Typography my={2} color="red" align='center'>
+          {loginError}
+        </Typography>
+        <Button
+          fullWidth="100%"
+          type="submit"
+          variant="contained"
+        >
+          Login
+        </Button>
+      </form>
+      <Box my={2}>
+        <Button
+          onClick={signInWithGoogle}
+          color='secondary'
+          fullWidth="100%"
+          type="submit"
+          variant="contained"
+        >
+          Sign in with Google
+        </Button>
+      </Box>
+    </Container>
   )
 }
 
