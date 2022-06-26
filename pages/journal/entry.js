4590e5e820d99164d14cf10e39c18cf506548baa
Iqbal955 <hamzah.iqbal@consultant.lego.com>
2022-06-26 15:14:21 +0200

    updated UI of nav bar in logged in

--- pages/journal/entry.js
+++ pages/journal/entry.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { useContext } from "react";
 import Link from "next/link";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import AppBar from "@material-ui/core/AppBar";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
